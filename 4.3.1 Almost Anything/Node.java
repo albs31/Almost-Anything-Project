7eaa65219bac6af78a06cb18deebbc4723a9de32
Andy Xu <tritonisatrident@gmail.com>
2026-04-27 19:55:51 -0700

    node class remade

--- 4.3.1 Almost Anything/Node.java
+++ 4.3.1 Almost Anything/Node.java
@@ -1,34 +1,32 @@
 class Node implements Comparable<Node>
 {
-    //Position/identifier variables
     public double x;
     public double y;
 
-    //g, h, f (cost estimates)
     public double g;
     public double h;
     public double f;
-
-    //Node relationships
+    
     public Node parent;
     public Edge[] connections = new Edge[3];
+    public boolean isActive = true;
+    public int nodeType = 0; // 0 for intersection, 1 for building, 2 for park
 
-    //generate seed from node location
-    public long getSeed(){
+    public long getSeed() 
+    {
         long bitsX = Double.doubleToLongBits(x);
         long bitsY = Double.doubleToLongBits(y);
-        long seed = (bitsX^(bitsY*31L));
+        long seed = (bitsX ^ (bitsY * 31L));
         return seed; 
     }
 
-
-    public Node (double x, double y)
+    public Node(double x, double y) 
     {
         this.x = x;
         this.y = y;
     }
 
-    public int compareTo(Node other)
+    public int compareTo(Node other) 
     {
         return Double.compare(this.f, other.f);
     }
