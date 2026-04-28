b1f4f04e489c5b6cabb982c4bb8a15dcc7c5484a
Andy Xu <tritonisatrident@gmail.com>
2026-04-27 19:54:40 -0700

    Edge blueprint done

--- 4.3.1 Almost Anything/Edge.java
+++ 4.3.1 Almost Anything/Edge.java
@@ -1,33 +1,25 @@
 import java.util.ArrayList;
 
-//traffic datas
-public class Edge{
+public class Edge 
+{
     public Node from;
     public Node to;
     public ArrayList<Car> cars;
 
-    public double getWeight()
+    public Edge(Node from, Node to) 
     {
-        return 1 + 0.1 * cars.size();
+        this.from = from;
+        this.to = to;
+        this.cars = new ArrayList<>();
     }
 
-    public double getDistance()
+    public double getDistance() 
     {
-        return Math.sqrt(
-            Math.pow((to.x - from.x), 2) + Math.pow((to.y - from.y), 2)
-        );
+        return Math.sqrt(Math.pow((to.x - from.x), 2) + Math.pow((to.y - from.y), 2));
     }
-    
-    //return num cars from startnode to endnode
-    public int getTraffic(Node from, Node to){
-      int x = 0;
-      long seed1 = to.getSeed();
-      long seed2 = from.getSeed();
-      long seed
-      this.from = from;
-      this.to = to;
-      x = ();
-    
-      return x;
+
+    public double getCost() 
+    {
+        return getDistance() + (cars.size() * 1.2);
     }
 }
