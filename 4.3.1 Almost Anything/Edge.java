e63a074c2292db7e82726167ef18ffaa1fae1b43
alfiya <147679431+albs31@users.noreply.github.com>
2026-04-29 10:49:19 -0700

    Update Edge.java

--- 4.3.1 Almost Anything/Edge.java
+++ 4.3.1 Almost Anything/Edge.java
@@ -1,11 +1,12 @@
 import java.util.ArrayList;
 
+//edge class
 public class Edge 
 {
     public Node from;
     public Node to;
     public ArrayList<Car> cars;
-
+//make edge between two nodes
     public Edge(Node from, Node to) 
     {
         this.from = from;
@@ -13,11 +14,13 @@ public class Edge
         this.cars = new ArrayList<>();
     }
 
+//solve for line length
     public double getDistance() 
     {
         return Math.sqrt(Math.pow((to.x - from.x), 2) + Math.pow((to.y - from.y), 2));
     }
 
+//solve for cost
     public double getCost() 
     {
         return getDistance() + (cars.size() * 1.2);
