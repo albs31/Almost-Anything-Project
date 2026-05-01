b5a8bf37cb70e3d3363c3d31030ad83ffd9b2d6a
Andy Xu <tritonisatrident@gmail.com>
2026-05-01 08:35:09 -0700

    Modify graph setup to accept user input

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 import java.io.IOException;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) throws IOException 
     {
-        //Initializes a new graph
-        Graph graph = new Graph(300, 2000, 1000);
+        //Initializes a new graph with nodes based on user input
+        Graph graph = new Graph(Integer.parseInt(JOptionPane.showInputDialog("Number of Nodes:")), 2000, 1000);
 
-        //Adds cars and roadblocks to the graph, then initializes the map
-        graph.addCars(500);
-        graph.addRoadblocks(10);
+        //Adds cars and roadblocks based on user input to the graph, then initializes the map
+        graph.addCars(Integer.parseInt(JOptionPane.showInputDialog("Number of Cars:")));
+        graph.addRoadblocks(Integer.parseInt(JOptionPane.showInputDialog("Number of Roadblocks:")));
+        
         Map map = new Map(graph);
     }
 }
